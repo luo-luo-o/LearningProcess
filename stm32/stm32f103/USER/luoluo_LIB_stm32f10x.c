1a5e47eba17cb766b83a0ad721fb43bd2fa532a4
Luo_luo_o <luo_luo_o@outlook.com>
2025-10-02 21:18:51 +0800

    stm32: 简易流水灯的实现（利用按键控制流水灯的流动方向）
    add .gitignore
    
    1

---
+++ stm32/stm32f103/USER/luoluo_LIB_stm32f10x.c
@@ -0,0 +1,4 @@
+#include <luoluo_LIB_stm32f10x.h>
+
+
+// End of file
---
+++ stm32/stm32f103/USER/luoluo_LIB_stm32f10x.h
@@ -0,0 +1,29 @@
+/**
+  ******************************************************************************
+  * @file   luoluoLIB_stm32f103.h
+  * @author luo_luo
+  * @brief  stm32f103二次封装
+  ******************************************************************************
+  * 
+  */
+
+#ifndef LUOLUOLIB_STM32F103_H
+#define LUOLUOLIB_STM32F103_H
+
+/*  Include necessary headers  */ 
+#include <stm32f10x.h>
+#include <Delay.h>
+#include <LED.h>
+#include <Key.h>
+
+/*  End Include  */ 
+
+
+    /*  GPIO   */
+    #define GPIO_PIN_(num) (1<<num)
+
+
+
+
+#endif // LUOLUOLIB_H
+// End of file
